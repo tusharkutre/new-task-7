acab6cd680ca60f5dc2fea3bab896b3855b21b69
Tushar Kutre <115899383+tusharkutre@users.noreply.github.com>
2024-07-26 11:43:49 +0530

    task 7 files added
    classes and objects

---
+++ MainMethod.java
@@ -0,0 +1,18 @@
+class MainMethod{
+
+public oil(){
+	System.out.println("hey im oil");
+}
+
+public static void main(String[] args){
+	
+	System.out.println("start main..");
+	
+	new Water();
+	
+	new oil();
+	System.out.println("End main");
+	
+}
+
+}
---
+++ water.java.bak
@@ -0,0 +1,8 @@
+class Water
+{
+
+public Water(){
+	System.out.println("hey im water");
+}
+
+}
